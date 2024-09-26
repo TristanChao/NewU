4d7b89ef841dee3fa5b75f1f5c182d376b60930c
Tristan Chao <chaot372@gmail.com>
2024-09-26 00:14:58 +0000

    started setup

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,46 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId"          serial          PRIMARY KEY,
+  "username"        text            not null,
+  "hashedPassword"  text            not null,
+  "displayName"     text            not null,
+  "createdAt"       timestamptz(6)  not null default now(),
+  "updatedAt"       timestamptz(6)  not null default now(),
+  unique("username")
+);
+
+CREATE TABLE "calendars" (
+  "calendarId"  serial          PRIMARY KEY,
+  "owner"       integer         not null,
+  "type"        text            not null,
+  "name"        text            not null,
+  "color"       text            not null,
+  "desc"        text            not null,
+  "createdAt"   timestamptz(6)  not null default now(),
+  "updatedAt"   timestamptz(6)  not null default now()
+);
+
+CREATE TABLE "calendarAccess" (
+  "calendarId"  integer         not null,
+  "userId"      integer         not null,
+  "updatedAt"   timestamptz(6)  not null default now()
+);
+
+CREATE TABLE "habitMarks" (
+  "markId"      serial          PRIMARY KEY,
+  "calendarId"  integer         not null,
+  "date"        text            not null,
+  "isCompleted" boolean         not null,
+  "updatedAt"   timestamptz(6)  not null default now()
+);
+
+ALTER TABLE "calendars" ADD FOREIGN KEY ("owner") REFERENCES "users" ("userId");
+
+ALTER TABLE "calendarAccess" ADD FOREIGN KEY ("calendarId") REFERENCES "calendars" ("calendarId");
+
+ALTER TABLE "calendarAccess" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "habitMarks" ADD FOREIGN KEY ("calendarId") REFERENCES "calendars" ("calendarId");
