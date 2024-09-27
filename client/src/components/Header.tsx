fae305fbae79dbe9f56938d5c8665697f7fc8eea
Tristan Chao <chaot372@gmail.com>
2024-09-27 23:55:56 +0000

    stylistic changes. app title in header changed to a link that goes to the home page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { TiThMenu } from 'react-icons/ti';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { useUser } from './useUser';
 import { Popup } from './Popup';
 import { useRef, useState } from 'react';
@@ -18,7 +18,12 @@ export function Header() {
     <>
       <div>
         <div className="p-2 px-[15px] flex justify-between">
-          <h1 className="text-[32px] font-bold">NewU</h1>
+          <Link
+            to="/"
+            className="text-[32px] font-bold cursor-pointer"
+            style={{ fontFamily: 'Arvo, serif' }}>
+            NewU
+          </Link>
           {user && (
             <div className="flex items-center">
               <h1 className="h-fit mr-[20px] text-lg inline-block">
